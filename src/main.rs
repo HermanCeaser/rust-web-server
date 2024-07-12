be5f283fbbde4ef1f05366491046438e27b9b3b8
Herman Ceaser <ceaserbanks09@gmail.com>
2024-07-12 10:46:53 +0300

    server responds with 200

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +12,11 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let write_all = _stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
+                let _ = write_all;
+
             }
             Err(e) => {
                 println!("error: {}", e);
