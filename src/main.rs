3ba0976f62206d9f4ba010b8784b2beb83567e2f
Herman Ceaser <ceaserbanks09@gmail.com>
2024-07-12 13:29:15 +0300

    Passes stage 4: Added request path matching

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ fn main() {
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
@@ -27,12 +26,25 @@ fn handle_connection(mut stream: TcpStream) {
     let buffer = BufReader::new(&mut stream);
     let request_line = buffer.lines().next().unwrap().unwrap();
 
-    let status_line = if request_line == "GET / HTTP/1.1" {
-        "HTTP/1.1 200 OK\r\n\r\n"
+    let (status_line, body) = if request_line.starts_with("GET /echo/") {
+        let echo_content = &request_line[10..request_line.len()-9];
+        // println!("{:#?}", echo_content);
+        ("HTTP/1.1 200 OK", echo_content)
     } else {
-        "HTTP/1.1 404 Not Found\r\n\r\n"
+        match &request_line[..] {
+            "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", ""),
+            _ => ("HTTP/1.1 404 Not Found", ""),
+        }
     };
 
-    
-    stream.write_all(status_line.as_bytes()).expect("200 OK\n");
+   
+    let response = if body.is_empty() {
+        format!("{status_line}\r\n\r\n")
+    } else {
+        let content_length = body.len();
+        let content_type = "text/plain"; // Assuming plain text; adjust as necessary
+        format!("{status_line}\r\nContent-Length: {content_length}\r\nContent-Type: {content_type}\r\n\r\n{body}")
+    };
+
+    stream.write_all(response.as_bytes()).expect("200 OK\n");
 }
